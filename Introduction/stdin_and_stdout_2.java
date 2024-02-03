d97515c708193c0e97bd788f6ffc7f8d285f3ddd
MoldyPuG <kyohei.work23@gmail.com>
2024-02-03 16:33:20 +0900

    create - stdin_and_stdout_2

---
+++ Introduction/stdin_and_stdout_2.java
@@ -0,0 +1,17 @@
+import java.util.Scanner;
+
+public class stdin_and_stdout_2 {
+    public static void main(String[] args) {
+        Scanner scan = new Scanner(System.in);
+        int i = scan.nextInt();
+        double d = scan.nextDouble();
+        scan.nextLine(); // This is needed to clear the buffer
+        String s = scan.nextLine();
+
+        scan.close();
+
+        System.out.println("String: " + s);
+        System.out.println("Double: " + d);
+        System.out.println("Int: " + i);
+    }
+}
